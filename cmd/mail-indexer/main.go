c5c61d866143e8d17f7c85ef0741b81a2b9a9f29
Felix Sanchez <K90009968@kimballelectronics.com>
2022-10-19 09:00:27 -0500

    moved sample data to zinc api folder

--- cmd/mail-indexer/main.go
+++ cmd/mail-indexer/main.go
@@ -1,11 +1,43 @@
 package main
 
-import (
-	"github.com/FelixSnz/mail-indexer/internal/setup"
-)
-
 func main() {
 	//fmt.Println(zinc.Search("Content", "manipulated"))
+	// r := chi.NewRouter()
+	// r.Use(middleware.Logger)
+	// r.Post("http://localhost:4080/api/emails/_search", func(w http.ResponseWriter, r *http.Request) {
 
-	setup.Run(`assets\email_samples`, "email_samples.json")
+	// })
+
+	// r.Post()
+
+	// http.ListenAndServe(":4080", r)
+
+	// query := fmt.Sprintf(`{
+	// 	"search_type": "querystring",
+	// 	"query":
+	// 	{
+	// 		"term": "%s:%s",
+	// 	},
+	// 	"_source": []
+	// }`, key, pattern)
+
+	// req, err := http.NewRequest("POST", "", strings.NewReader(query))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// req.SetBasicAuth("admin", "Complexpass#123")
+	// req.Header.Set("Content-Type", "application/json")
+	// req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+	// resp, err := http.DefaultClient.Do(req)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
+	// log.Println(resp.StatusCode)
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// return string(body)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FelixSnz/mail-indexer
 
 go 1.19
+
+require github.com/go-chi/chi v1.5.4
