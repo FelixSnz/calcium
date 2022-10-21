b3a3054b43f7f04898b498668353b4576b316642
Felix Sanchez <K90009968@kimballelectronics.com>
2022-10-20 21:20:36 -0500

    add search by data range and add document functions

--- pkg/zinc/zincsearch.go
+++ pkg/zinc/zincsearch.go
@@ -6,9 +6,41 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func Search(field, pattern string) string {
+func AddDoc(json_str string) {
+
+	index_url := "http://localhost:4080/api/manual_emails/"
+	api_call := "_doc"
+	fmt.Println("HTTP JSON POST URL:", index_url+api_call)
+
+	req, err := http.NewRequest("POST", index_url+api_call, strings.NewReader(json_str))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth("admin", "Complexpass#123")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+
+	log.Println(resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(body)
+	//return string(body)
+
+}
+
+func SearchByTextField(field, text string) string {
 
 	index_url := "http://localhost:4080/api/emails/"
 	api_call := "_search"
@@ -23,7 +55,7 @@ func Search(field, pattern string) string {
         "from": 0,
         "max_results": 20,
         "_source": []
-    }`, field, pattern)
+    }`, field, text)
 	req, err := http.NewRequest("POST", index_url+api_call, strings.NewReader(query))
 	if err != nil {
 		log.Fatal(err)
@@ -45,4 +77,43 @@ func Search(field, pattern string) string {
 		log.Fatal(err)
 	}
 	return string(body)
+}
+
+func SearchByDataRange(from, to time.Time) string {
+
+	index_url := "http://localhost:4080/api/emails/"
+	api_call := "_search"
+	fmt.Println("HTTP JSON POST URL:", index_url+api_call)
+
+	query := fmt.Sprintf(`{
+		"search_type": "querystring",
+		"query": {
+			"term": '+date:>"%s" +date:<"%s"'
+		},
+	
+	
+		"_source":["date"]
+	}`, from.String(), to.String())
+	req, err := http.NewRequest("POST", index_url+api_call, strings.NewReader(query))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth("admin", "Complexpass#123")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+
+	log.Println(resp.StatusCode)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(body)
+
 }
