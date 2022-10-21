1d8e4ff6c27e9b83b9a38d529323b9d049ee6184
Felix Sanchez <K90009968@kimballelectronics.com>
2022-10-21 11:34:58 -0500

    refactored email pkg with OOP paradigm

--- internal/email/mail.go
+++ internal/email/mail.go
@@ -1,16 +1,13 @@
-// Package for describing email structure thus handling emails
-// with the general porpuse of generating a json file from
-// a directory of email files
+// Package for describing email object thus handling email files
+// with the general porpuse of generating a the json format of the email file
 package email
 
 import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -18,52 +15,75 @@ import (
 	"github.com/FelixSnz/mail-indexer/internal/utils"
 )
 
-var root string
-
-// Struct to describe header for all the records/emails
-type EmailsDir struct {
-	Index   string   `json:"index"`
-	Records []*Email `json:"records"`
-}
-
 // defines Email file as an object to store its information as json format
-type Email struct {
+type JsonStruct struct {
 	Origin      string    `json:"origin"`
 	SubFolder   string    `json:"subfolder"`
 	Id          string    `json:"id"`
-	Date        time.Time `json:"@date"`
+	Date        time.Time `json:"date"`
 	From        string    `json:"from"`
 	To          []string  `json:"to"`
 	Cc          []string  `json:"cc"`
 	Bcc         []string  `json:"bcc"`
 	Subject     string    `json:"subject"`
-	Version     string    `json:"version"`
+	Version     string    `json:"mimeversion"`
 	ContentType string    `json:"type"`
 	Encoding    string    `json:"encoding"`
 	Content     string    `json:"content"`
 }
 
-// defines scan states when reading email file line by line
-var States = [10]string{
-	"Message-ID: ",
-	"Date: ",
-	"From: ",
-	"To: ",
-	"Cc: ",
-	"Bcc: ",
-	"Subject: ",
-	"Mime-Version: ",
-	"Content-Type: ",
-	"Content-Transfer-Encoding: ",
+type Email struct {
+	path   string
+	root   string
+	states [10]string // defines scan states when reading email file line by line
+}
+
+// constructor method to create a new email object
+func New(path, root string) *Email {
+	email := new(Email)
+	email.path = path
+	email.root = root
+
+	email.states = [10]string{
+		"Message-ID: ",
+		"Date: ",
+		"From: ",
+		"To: ",
+		"Cc: ",
+		"Bcc: ",
+		"Subject: ",
+		"Mime-Version: ",
+		"Content-Type: ",
+		"Content-Transfer-Encoding: ",
+	}
+	return email
+}
+
+// returns the message of the email
+func (email Email) GetMessage() string {
+
+	raw_data, err := os.ReadFile(email.path)
+
+	if err != nil {
+		fmt.Printf("error: Couldn't read file at path: %s\n", email.path)
+		log.Fatal(err)
+	}
+
+	//to get the message content of the email file, separates
+	//the whole content by a regular expression
+	reg_delimeter := regexp.MustCompile(`X-FileName\s*(.*?)\s*\n`)
+	content := reg_delimeter.Split(string(raw_data), -1)[1]
+	return content
+
 }
 
 // by a given email file line, returns the scan state if found, otherwise, returns the provided previus state
-func GetState(line, prev_state string) string {
+func (email Email) GetState(line, prev_state string) string {
 
-	for i := 0; i < len(States); i++ {
+	for i := 0; i < len(email.states); i++ {
 
-		if strings.HasPrefix(line, States[i]) {
-			return States[i]
+		if strings.HasPrefix(line, email.states[i]) {
+			return email.states[i]
 		}
 	}
 
@@ -71,89 +91,11 @@ func GetState(line, prev_state string) string {
 
 }
 
-// by a given string of email adresses, removes spaces, tabs, new lines and returns an array
-// where each element is an email address
-func GetMails(mails_string string) []string {
-
-	// temp_line :=
-
-	return strings.FieldsFunc(
-		strings.TrimFunc(
-			strings.Replace(
-				strings.Replace(mails_string,
-					" ", "", -1), "\t", "", -1), func(c rune) bool {
-				return c == 9 || c == 10 || c == 11
-			}),
-		func(c rune) bool {
-			return c == ','
-		})
-
-}
-
-// by a given directory path of email files, processes each one and
-// creates a json file
-func DirToJson(path, save_name string) {
-	var emails []*Email
-
-	if strings.Contains(path, "\\") {
-		splitted_path := strings.Split(path, "\\")
-		root = splitted_path[len(splitted_path)-1]
-	} else {
-		root = path
-	}
-
-	filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-		if !d.IsDir() {
-
-			email := PathToEmail(path)
-			emails = append(emails, &email)
-		}
-		return nil
-	})
-
-	p, _ := json.Marshal(EmailsDir{Index: root, Records: emails})
-	_ = os.WriteFile(save_name, p, 0644)
-
-}
-
-func CleanDate(raw_date string) time.Time {
-
-	str_date := strings.TrimPrefix(raw_date, "Date: ")
-	day := str_date[5:7]
-	no_zero_pattern := `\d{1} `
-	match, err := regexp.MatchString(no_zero_pattern, day)
-
+// by the path of the emil instance, returns a json string
+func (email Email) GetJson() string {
+	readFile, err := os.Open(email.path)
 	if err != nil {
-		fmt.Println("error matching day string")
-		log.Fatal(err)
-	}
-
-	if match {
-
-		index := 5
-		str_date = str_date[:index] + "0" + str_date[index:]
-	}
-
-	num_zone_regex := regexp.MustCompile(`-\s*(\d{4}) |\(|\)`)
-	str_date = num_zone_regex.ReplaceAllString(str_date, "")
-	date, err := time.Parse(time.RFC1123, str_date)
-
-	if err != nil {
-		fmt.Println("error parsing date")
-		log.Fatal(err)
-	}
-	return date
-}
-
-// by a given email file path, returns an Email object
-func PathToEmail(path string) Email {
-
-	readFile, err := os.Open(path)
-	if err != nil {
-		fmt.Printf("couldn't open email file at path: %s\n", path)
+		fmt.Printf("couldn't open email file at path: %s\n", email.path)
 		panic(err)
 
 	}
@@ -179,7 +121,7 @@ func PathToEmail(path string) Email {
 
 		line := fileScanner.Text()
 
-		state := GetState(line, prev_state)
+		state := email.GetState(line, prev_state)
 
 		if !strings.HasPrefix(line, "X-") {
 
@@ -229,31 +171,18 @@ func PathToEmail(path string) Email {
 
 	readFile.Close()
 
-	splited_path := strings.Split(path, "\\")
+	splited_path := strings.Split(email.path, "\\")
 
-	root_idx := utils.IndexOf(root, splited_path)
+	root_idx := utils.IndexOf(email.root, splited_path)
 
 	if root_idx == -1 {
 		log.Fatal("couldn't extract the root index")
 	}
 
 	sub_folder := strings.Join(splited_path[root_idx+2:len(splited_path)-1], `\`)
-	raw_content, err := os.ReadFile(path)
+	email_msg := email.GetMessage()
 
-	if err != nil {
-		fmt.Printf("couldn't read file at path: %s\n", path)
-		panic(err)
-
-	}
-
-	//to get the message content of the email file, separates
-	//the whole content by a regular expression
-
-	reg_delimeter := regexp.MustCompile(`X-FileName\s*(.*?)\s*\n`)
-	raw_content_str := string(raw_content)
-	content := reg_delimeter.Split(raw_content_str, -1)
-
-	return Email{
+	json_str, err := json.Marshal(JsonStruct{
 		Origin:      splited_path[root_idx+1],
 		SubFolder:   sub_folder,
 		Id:          strings.TrimPrefix(ID, "Message-ID: "),
@@ -266,6 +195,62 @@ func PathToEmail(path string) Email {
 		Version:     strings.TrimPrefix(mime_version, "Mime-Version: "),
 		ContentType: strings.TrimPrefix(content_type, "Content-Type: "),
 		Encoding:    strings.TrimPrefix(encoding, "Content-Transfer-Encoding: "),
-		Content:     content[1],
+		Content:     email_msg,
+	})
+
+	if err != nil {
+		fmt.Printf("error: Couldn't create json file at path: %s\n", email.path)
+		log.Fatal(err)
 	}
+
+	return string(json_str)
+
+}
+
+// by a given string of email adresses, removes spaces, tabs, new lines and returns an array
+// where each element is an email address
+func GetMails(mails_string string) []string {
+
+	return strings.FieldsFunc(
+		strings.TrimFunc(
+			strings.Replace(
+				strings.Replace(mails_string,
+					" ", "", -1), "\t", "", -1), func(c rune) bool {
+				return c == 9 || c == 10 || c == 11
+			}),
+		func(c rune) bool {
+			return c == ','
+		})
+
+}
+
+// by a raw date extracted from the email content, cleans the format and returns
+// the date in the default time type for go
+func CleanDate(raw_date string) time.Time {
+
+	str_date := strings.TrimPrefix(raw_date, "Date: ")
+	day := str_date[5:7]
+	no_zero_pattern := `\d{1} `
+	match, err := regexp.MatchString(no_zero_pattern, day)
+
+	if err != nil {
+		fmt.Println("error matching day string")
+		log.Fatal(err)
+	}
+
+	if match {
+
+		index := 5
+		str_date = str_date[:index] + "0" + str_date[index:]
+	}
+
+	num_zone_regex := regexp.MustCompile(`-\s*(\d{4}) |\(|\)`)
+	str_date = num_zone_regex.ReplaceAllString(str_date, "")
+	date, err := time.Parse(time.RFC1123, str_date)
+
+	if err != nil {
+		fmt.Println("error parsing date")
+		log.Fatal(err)
+	}
+	return date
 }
--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -1,12 +1,37 @@
 package setup
 
 import (
-	"fmt"
+	"io/fs"
+	"log"
+	"path/filepath"
+	"strings"
 
 	"github.com/FelixSnz/mail-indexer/internal/email"
+	"github.com/FelixSnz/mail-indexer/pkg/zinc"
 )
 
-func Run(emails_path, json_savename string) {
-	fmt.Println("Running setup data")
-	email.DirToJson(emails_path, json_savename)
+func UploadDocuments(emails_path string) {
+
+	var root string
+
+	if strings.Contains(emails_path, "\\") {
+		splitted_path := strings.Split(emails_path, "\\")
+		root = splitted_path[len(splitted_path)-1]
+	} else {
+		root = emails_path
+	}
+
+	filepath.WalkDir(emails_path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		if !d.IsDir() {
+
+			email := email.New(path, root)
+			json := email.GetJson()
+			zinc.AddDoc(json)
+		}
+		return nil
+	})
+
 }
