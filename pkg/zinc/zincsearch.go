061c002fd3dcfa57c3bff033ee71fd80ac7ab97f
Felix Sanchez <K90009968@kimballelectronics.com>
2022-10-23 12:52:33 -0500

    catch zincsearch api errors

--- pkg/zinc/zincsearch.go
+++ pkg/zinc/zincsearch.go
@@ -2,12 +2,17 @@
 package zinc
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"strings"
 )
+
+type ErrorResponse struct {
+	Err string `json:"error"`
+}
 
 type zincIndex struct {
 	name string
@@ -29,7 +34,7 @@ func NewIndex(name string) *zincIndex {
 //	{
 //		"search_type": "querystring",
 //		"query": {
-//			"term": '+date:>"%s" +date:<"%s"'
+//			"term": '+date:>"2000-01-01T00:00:00Z" +date:<"2000-12-28T00:00:00Z"'
 //		},
 //
 //
@@ -42,7 +47,7 @@ func NewIndex(name string) *zincIndex {
 //		"search_type": "querystring",
 //		"query":
 //		{
-//			"term": "%s:%s"
+//			"term": "content:manipulated"
 //		},
 //		"from": 0,
 //		"max_results": 20,
@@ -66,14 +71,26 @@ func (index zincIndex) Post(method, json_str string) string {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	log.Printf("resp: %d\n", resp.StatusCode)
 
-	log.Println(resp.StatusCode)
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if resp.StatusCode != 200 {
+
+		var err_resp ErrorResponse
+		json.Unmarshal(body, &err_resp)
+
+		fmt.Println("error: ")
+		log.Fatalf(err_resp.Err)
+
+	}
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+
 	fmt.Println(body)
 	return string(body)
 
