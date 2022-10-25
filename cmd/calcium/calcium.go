610efc6b0f166a743cc972381fe9222dcc799aa8
FelixSnz <felixsanchez0124@gmail.com>
2022-10-25 10:02:22 -0500

    basic HTML template

--- cmd/calcium/calcium.go
+++ cmd/calcium/calcium.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi"
 )
@@ -28,6 +30,10 @@ func main() {
 		w.Write([]byte("Hello World!"))
 	})
 
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, `web\public`))
+
+	FileServer(router, "/files", filesDir)
 	router.Get("/home", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte("you are in home"))
@@ -35,4 +41,23 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
