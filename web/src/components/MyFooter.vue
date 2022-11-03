ef6b0edf3a4d137e0b82c4851cd2d8402b0fe06b
FelixSnz <felixsanchez0124@gmail.com>
2022-11-02 21:39:30 -0500

    basic footer vue component

---
+++ web/src/components/MyFooter.vue
@@ -0,0 +1,48 @@
+<template>
+
+<div class="bg-slate-50">
+			<div class="max-w-2xl mx-auto p-8">
+
+				<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+					<div class="sm:flex sm:items-center sm:justify-between">
+						<a href="#" target="_blank" class="flex items-center mb-4 sm:mb-0">
+							<img src="../assets/noun-milk.png" class="mr-4 h-8" alt="Calcium Logo" />
+							<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Calcium</span>
+						</a>
+						<ul class="flex flex-wrap items-center mb-6 sm:mb-0">
+							<li>
+								<a href="#" class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">About</a>
+							</li>
+							<li>
+								<a href="#" class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Privacy
+									Policy</a>
+							</li>
+							<li>
+								<a href="#" class="text-sm text-gray-500 hover:underline dark:text-gray-400">Contact</a>
+							</li>
+						</ul>
+					</div>
+					<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+					<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" target="_blank" class="hover:underline">Calcium™</a>. All Rights Reserved.
+				</span>
+				</footer>
+			</div>
+
+		</div>
+    </template>
+    
+    <script lang="ts">
+    import { defineComponent } from 'vue';
+    
+    
+    //or in file components
+    
+    
+    export default defineComponent({
+        data() {
+            return {
+    
+            }
+        },
+    })
+    </script>
