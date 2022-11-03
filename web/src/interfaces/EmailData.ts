9a51d2ed6c068c7124ef98ca2640daacb3b83ba0
FelixSnz <felixsanchez0124@gmail.com>
2022-11-02 21:35:12 -0500

    interface to receive server email data

---
+++ web/src/interfaces/EmailData.ts
@@ -0,0 +1,10 @@
+export interface EmailData {
+    origin: string,
+    subfolder: string,
+    date: Date,
+    subject: string,
+    from: string,
+    to: string,
+    cc: string,
+    content:string
+}
